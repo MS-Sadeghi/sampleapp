fc249d28444d8ab55d0363994a4ae1f2ca427cf3
Masoud <MasoudSadeghi.Mail@gmail.com>
2021-12-17 01:11:25 +0330

    connect to local db

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 // index.js
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const task = require("./models/model.js");
+
 const app = express();
 const port = process.env.PORT || 5000;
-const mongoose = require('mongoose');
-const task = require('./models/model.js');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const cors = require('cors');
-app.use(cors());
-
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-// Replace the following with your Atlas connection string                                                                                                                                        
+// Replace the following with your Atlas connection string
 const connectionUrl = process.env.MONGODB_URI;
-mongoose.connect(connectionUrl, { 
-    useNewUrlParser: true,
-    useUnifiedTopology: true 
-  }); 
+mongoose.connect(connectionUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //import routes
-const routes = require('./routes/routes.js'); 
+const routes = require("./routes/routes.js");
 
 //register the route
 routes(app);
 
-app.listen(port, ()=> {
+app.listen(port, () => {
   console.log(`RESTful API server running on ${port}`);
 });
